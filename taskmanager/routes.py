08ba54064f43a65541505d448dae8f098dcd3f28
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-10-22 19:22:56 +0000

    add edit_cat html file and backend functionality to update database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -14,11 +14,20 @@ def categories():
     return render_template("categories.html", categories=categories)
 
 
-@app.route("/add_cateogry", methods=["GET", "POST"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
-    return render_template("add_cateogry.html")
+    return render_template("add_category.html")
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
